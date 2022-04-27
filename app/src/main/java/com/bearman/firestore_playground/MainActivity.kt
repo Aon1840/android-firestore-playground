fb5b6c602f1661714b261c26cd0859ca9a273feb
Thanes Sriamornruttanakul <benz.thanes@gmail.com>
2022-04-27 11:15:41 +0700

    test get data specific id

--- app/src/main/java/com/bearman/firestore_playground/MainActivity.kt
+++ app/src/main/java/com/bearman/firestore_playground/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.bearman.firestore_playground
 
 import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btSeeMore: Button
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btSeeMore = findViewById(R.id.btSeeMore)
+        textView = findViewById(R.id.textView)
 
         val db = Firebase.firestore
 
@@ -36,6 +39,31 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Error adding document", e)
             }
 
+//        // get 1 time
+//        val docRefGet = db.collection("BENZ").document("ldzUpM6jpO3zABXwSXzX")
+//        docRefGet.get()
+//            .addOnSuccessListener { result ->
+//                Log.d(TAG, "Current data get: ${result.data}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.d(TAG, "Current data get: null")
+//            }
+//
+//        // get first time and subscribe data when change
+//        val docRef = db.collection("BENZ").document("ldzUpM6jpO3zABXwSXzX")
+//        docRef.addSnapshotListener { snapshot, e ->
+//            if (e != null) {
+//                Log.w(TAG, "Listen failed.", e)
+//                return@addSnapshotListener
+//            }
+//
+//            if (snapshot != null && snapshot.exists()) {
+//                Log.d(TAG, "Current data: ${snapshot.data}")
+//                textView.text = snapshot.data.toString()
+//            } else {
+//                Log.d(TAG, "Current data: null")
+//            }
+//        }
 
         btSeeMore.setOnClickListener {
             startActivity(AllUserActivity.createIntent(this@MainActivity))
